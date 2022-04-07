3719eb85eff3cade8de07a878edf900fb4873203
Ali Behjati <bahjatia@gmail.com>
2022-04-07 15:26:50 +0000

    Add pre-commit hooks

--- examples/terra-contract/src/contract.rs
+++ examples/terra-contract/src/contract.rs
@@ -40,9 +40,9 @@ pub fn instantiate(
     _info: MessageInfo,
     msg: InstantiateMsg,
 ) -> StdResult<Response> {
-    // It is a good practice that your contract stores the pyth contract address and ids of the price feeds
-    // it needs upon instantiation or by an authorized approach. This will ensure that a wrong address won't
-    // be used.
+    // It is a good practice that your contract stores the pyth contract address and ids of the
+    // price feeds it needs upon instantiation or by an authorized approach. This will ensure
+    // that a wrong address won't be used.
     let state = State {
         pyth_contract_addr: deps.api.addr_validate(msg.pyth_contract_addr.as_ref())?,
         price_feed_id:      msg.price_feed_id,
@@ -76,9 +76,10 @@ fn query_fetch_price(deps: Deps) -> StdResult<FetchPriceResponse> {
     let state = STATE.load(deps.storage)?;
 
     // query_price_feed is the standard way to read the current price from a Pyth price feed.
-    // It takes the address of the Pyth contract (which is fixed for each network) and the id of the price feed.
-    // The result is a PriceFeed object with fields for the current price and other useful information.
-    // The function will fail if the contract address or price feed id are invalid.
+    // It takes the address of the Pyth contract (which is fixed for each network) and the id of the
+    // price feed. The result is a PriceFeed object with fields for the current price and other
+    // useful information. The function will fail if the contract address or price feed id are
+    // invalid.
     let price_feed = query_price_feed(
         &deps.querier,
         state.pyth_contract_addr.into_string(),
@@ -88,11 +89,11 @@ fn query_fetch_price(deps: Deps) -> StdResult<FetchPriceResponse> {
 
     // Get the current price and confidence interval from the price feed.
     // This function returns None if the price is not currently available.
-    // This condition can happen for various reasons. For example, some products only trade at specific times, or
-    // network outages may prevent the price feed from updating.
+    // This condition can happen for various reasons. For example, some products only trade at
+    // specific times, or network outages may prevent the price feed from updating.
     //
-    // The example code below throws an error if the price is not available. It is recommended that you handle
-    // this scenario more carefully. Consult the [consumer best practices](https://docs.pyth.network/consumers/best-practices)
+    // The example code below throws an error if the price is not available. It is recommended that
+    // you handle this scenario more carefully. Consult the [consumer best practices](https://docs.pyth.network/consumers/best-practices)
     // for recommendations.
     let current_price = price_feed
         .get_current_price()
