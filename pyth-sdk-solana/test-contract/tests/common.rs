8d4005f645e3b749983da577399e657c5701bddb
Ali Behjati <bahjatia@gmail.com>
2022-05-12 15:55:08 +0000

    Use saturating_sub instead of overflowing sub
    Also adds tests

--- pyth-sdk-solana/test-contract/tests/common.rs
+++ pyth-sdk-solana/test-contract/tests/common.rs
@@ -7,11 +7,17 @@ use test_contract::processor::process_instruction;
 
 // Panics if running instruction fails
 pub async fn test_instr_exec_ok(instr: Instruction) {
-    let (mut banks_client, payer, recent_blockhash) =
-        ProgramTest::new("test_contract", id(), processor!(process_instruction))
-            .start()
-            .await;
-    let mut transaction = Transaction::new_with_payer(&[instr], Some(&payer.pubkey()));
-    transaction.sign(&[&payer], recent_blockhash);
-    banks_client.process_transaction(transaction).await.unwrap()
+    let mut context = ProgramTest::new("test_contract", id(), processor!(process_instruction))
+        .start_with_context()
+        .await;
+
+    context.warp_to_slot(1000).unwrap();
+
+    let mut transaction = Transaction::new_with_payer(&[instr], Some(&context.payer.pubkey()));
+    transaction.sign(&[&context.payer], context.last_blockhash);
+    context
+        .banks_client
+        .process_transaction(transaction)
+        .await
+        .unwrap()
 }
