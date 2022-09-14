3a03ac30926d912405d543f0152f7e28bd0bff47
Yunhao Zhang <yz2327@cornell.edu>
2022-09-13 20:52:22 -0400

    Add scripts for invocation in examples/sol-contract

--- examples/sol-contract/scripts/invoke.ts
+++ examples/sol-contract/scripts/invoke.ts
@@ -1,11 +1,11 @@
-const {struct, u8} = require("@solana/buffer-layout");
 const web3 = require("@solana/web3.js");
+const {struct, u8} = require("@solana/buffer-layout");
 
 export const invoke = async (loan: string, collateral: string) => {
     /* Airdrop */
     console.log("Airdroping...");
-    let keypair = web3.Keypair.generate();
     let payer = web3.Keypair.generate();
+    let keypair = web3.Keypair.generate();
     let conn = new web3.Connection(web3.clusterApiUrl('devnet'));
     let airdropSig = await conn.requestAirdrop(
         payer.publicKey,
@@ -13,7 +13,7 @@ export const invoke = async (loan: string, collateral: string) => {
     );
     await conn.confirmTransaction(airdropSig);
 
-    /* Prepare accounts */
+    /* Specify accounts being touched */
     const loanKey = new web3.PublicKey(loan);
     const collateralKey = new web3.PublicKey(collateral);
     let keys = [{pubkey: loanKey, isSigner: false, isWritable: false},
@@ -22,7 +22,7 @@ export const invoke = async (loan: string, collateral: string) => {
 
     /* Prepare parameters */
     let allocateStruct = {
-        index: 0,     // Loan2Value is instruction#0 in the program
+        index: 0,     // Loan2Value is instruction #0 in the program
         layout: struct([
             u8('instruction'),
         ])
@@ -38,7 +38,7 @@ export const invoke = async (loan: string, collateral: string) => {
     });
     tx.add(new web3.TransactionInstruction({
         keys,
-        programId: "F6SbH5bQZ8peCqQUDmpYqhUtDJVMcAe9JGLPMLGAesfy",
+        programId: "CD9PFy8satZh27JeEixunWjBqoYbpbLbUtZ6eWPbT1s6",
         data
     }));
 
@@ -46,7 +46,6 @@ export const invoke = async (loan: string, collateral: string) => {
     console.log("Confirmed TxHash: " + txSig);
 }
 
-let eth = "EdVCmQ9FSPcVe5YySXDPCRmc8aDQLKJ9xvYBMZPie1Vw";
-let usdc = "5SSkXsEKQepHHAewytPVwdej4epN1nxgLVM84L4KXgy7";
-let usdt = "38xoQ4oeJCBrcVvca2cGk7iV1dAfrmTR1kmhSCJQ8Jto";
-invoke(eth, usdt);
+let ethToUSD = "EdVCmQ9FSPcVe5YySXDPCRmc8aDQLKJ9xvYBMZPie1Vw";
+let usdtToUSD = "38xoQ4oeJCBrcVvca2cGk7iV1dAfrmTR1kmhSCJQ8Jto";
+invoke(ethToUSD, usdtToUSD);
--- examples/sol-contract/src/processor.rs
+++ examples/sol-contract/src/processor.rs
@@ -18,18 +18,19 @@ pub fn process_instruction(
     let instruction = PythClientInstruction::try_from_slice(input).unwrap();
     match instruction {
         PythClientInstruction::Loan2Value {} => {
-            // Suppose we have 1 loan token and 3000 collateral token
+            let loan = &_accounts[0];
+            let collateral = &_accounts[1];
+            msg!("The loan key is {}.", loan.key);
+            msg!("The collateral key is {}.", collateral.key);
+
+            msg!("Assume 1 unit of loan and 3000 unit of collateral.");
             let loan_cnt = 1;
             let collateral_cnt = 3000;
             
-            let loan = &_accounts[0];
-            msg!("The loan key is {}.", loan.key);
             let feed1 = load_price_feed_from_account_info(&loan).unwrap();
             let result1 = feed1.get_current_price().unwrap();
             let loan_value = result1.price * loan_cnt;
 
-            let collateral = &_accounts[1];
-            msg!("The collateral key is {}.", collateral.key);
             let feed2 = load_price_feed_from_account_info(&collateral).unwrap();
             let result2 = feed2.get_current_price().unwrap();
             let collateral_value = result2.price * collateral_cnt;
@@ -37,10 +38,10 @@ pub fn process_instruction(
             if collateral_value > loan_value {
                 msg!("Loan unit price is {}.", result1.price);
                 msg!("Collateral unit price is {}.", result2.price);
-                msg!("Collateral value is higher.");
+                msg!("The value of collateral is higher.");
                 Ok(())
             } else {
-                msg!("Loan value is higher!");
+                msg!("The value of loan is higher!");
                 Err(ProgramError::Custom(0))
             }
         }
