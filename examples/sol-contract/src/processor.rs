1b9a91e17a3900c6e8a69099918d02807690b649
Yunhao Zhang <yz2327@cornell.edu>
2022-09-13 21:26:34 -0400

    Add overflow checks in examples/sol-contract

--- examples/sol-contract/src/processor.rs
+++ examples/sol-contract/src/processor.rs
@@ -18,6 +18,7 @@ pub fn process_instruction(
     let instruction = PythClientInstruction::try_from_slice(input).unwrap();
     match instruction {
         PythClientInstruction::Loan2Value {} => {
+            // Parse the parameters and skip the necessary checks
             let loan = &_accounts[0];
             let collateral = &_accounts[1];
             msg!("The loan key is {}.", loan.key);
@@ -26,23 +27,35 @@ pub fn process_instruction(
             msg!("Assume 1 unit of loan and 3000 unit of collateral.");
             let loan_cnt = 1;
             let collateral_cnt = 3000;
-            
-            let feed1 = load_price_feed_from_account_info(&loan).unwrap();
-            let result1 = feed1.get_current_price().unwrap();
-            let loan_value = result1.price * loan_cnt;
 
-            let feed2 = load_price_feed_from_account_info(&collateral).unwrap();
-            let result2 = feed2.get_current_price().unwrap();
-            let collateral_value = result2.price * collateral_cnt;
+            // Calculate the value of the loan
+            let loan_value;
+            let feed1 = load_price_feed_from_account_info(&loan);
+            let result1 = feed1.unwrap().get_current_price().unwrap();
+            if let Some(v) = result1.price.checked_mul(loan_cnt) {
+                loan_value = v;
+            } else {
+                return Err(ProgramError::Custom(0))
+            }
+
+            // Calculate the value of the collateral
+            let collateral_value;
+            let feed2 = load_price_feed_from_account_info(&collateral);
+            let result2 = feed2.unwrap().get_current_price().unwrap();
+            if let Some(v) = result2.price.checked_mul(collateral_cnt) {
+                collateral_value = v;
+            } else {
+                return Err(ProgramError::Custom(0))
+            }
 
             if collateral_value > loan_value {
                 msg!("Loan unit price is {}.", result1.price);
                 msg!("Collateral unit price is {}.", result2.price);
                 msg!("The value of collateral is higher.");
-                Ok(())
+                return Ok(())
             } else {
                 msg!("The value of loan is higher!");
-                Err(ProgramError::Custom(0))
+                return Err(ProgramError::Custom(1))
             }
         }
     }
