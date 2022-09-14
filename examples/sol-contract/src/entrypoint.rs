685bffd74a397a9f46da5499266c98ada3d9c4bb
Yunhao Zhang <yz2327@cornell.edu>
2022-09-13 21:41:43 -0400

    Cleanup and add comments in examples/sol-contract

--- examples/sol-contract/src/entrypoint.rs
+++ examples/sol-contract/src/entrypoint.rs
@@ -5,6 +5,9 @@ use solana_program::pubkey::Pubkey;
 use solana_program::account_info::AccountInfo;
 use solana_program::entrypoint::ProgramResult;
 
+// Every solana program has an entry point function with 3 parameters:
+// the program ID, the accounts being touched by this program,
+// and an arbitrary byte array as the input data for execution.
 entrypoint!(process_instruction);
 fn process_instruction(
     program_id: &Pubkey,
--- examples/sol-contract/src/instruction.rs
+++ examples/sol-contract/src/instruction.rs
@@ -1,24 +1,11 @@
 //! Program instructions
 
-use crate::id;
-use borsh::{
-    BorshDeserialize,
-    BorshSerialize,
-};
-use solana_program::instruction::Instruction;
-use solana_program::instruction::AccountMeta;
+use borsh::BorshSerialize;
+use borsh::BorshDeserialize;
 
+// A solana program contains a number of instructions.
+// And this example contract contains only one instruction.
 #[derive(Clone, Debug, BorshSerialize, BorshDeserialize, PartialEq)]
 pub enum PythClientInstruction {
-    Loan2Value {},
-}
-
-pub fn loan_to_value(loan: AccountMeta, collateral: AccountMeta) -> Instruction {
-    Instruction {
-        program_id: id(),
-        accounts:   vec![loan, collateral],
-        data:       PythClientInstruction::Loan2Value {}
-        .try_to_vec()
-        .unwrap(),
-    }
+    Loan2Value {}, // In this enum, Loan2Value is number 0.
 }
