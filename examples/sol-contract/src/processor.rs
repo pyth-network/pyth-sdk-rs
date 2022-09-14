b718bb5cf127fd1d4cdfb251ad197b1a08bccb3f
Yunhao Zhang <yz2327@cornell.edu>
2022-09-14 10:47:42 -0400

    Remove the unwrap() in the code of examples/sol-contract

--- examples/sol-contract/src/processor.rs
+++ examples/sol-contract/src/processor.rs
@@ -17,8 +17,8 @@ pub fn process_instruction(
 ) -> ProgramResult {
     // Checking the validity of parameters is important.
     // This important step is skipped in this example contract.
-    let instruction = PythClientInstruction::try_from_slice(input);
-    match instruction.unwrap() {
+    let instruction = PythClientInstruction::try_from_slice(input)?;
+    match instruction {
         PythClientInstruction::Loan2Value {} => {
             let loan = &_accounts[0];
             let collateral = &_accounts[1];
@@ -31,25 +31,25 @@ pub fn process_instruction(
             // Calculate the value of the loan.
             // Pyth is called to get the unit price of the loan.
             let loan_value;
-            let feed1 = load_price_feed_from_account_info(&loan);
-            let result1 = feed1.unwrap().get_current_price().unwrap();
+            let feed1 = load_price_feed_from_account_info(loan)?;
+            let result1 = feed1.get_current_price().ok_or(ProgramError::Custom(0))?;
             if let Some(v) = result1.price.checked_mul(loan_cnt) {
                 loan_value = v;
             } else {
                 // An overflow occurs for result1.price * loan_cnt.
-                return Err(ProgramError::Custom(0))
+                return Err(ProgramError::Custom(1))
             }
 
             // Calculate the value of the collateral.
             // Pyth is called to get the unit price of the collateral.
             let collateral_value;
-            let feed2 = load_price_feed_from_account_info(&collateral);
-            let result2 = feed2.unwrap().get_current_price().unwrap();
+            let feed2 = load_price_feed_from_account_info(collateral)?;
+            let result2 = feed2.get_current_price().ok_or(ProgramError::Custom(0))?;
             if let Some(v) = result2.price.checked_mul(collateral_cnt) {
                 collateral_value = v;
             } else {
                 // An overflow occurs for result2.price * collateral_cnt.
-                return Err(ProgramError::Custom(0))
+                return Err(ProgramError::Custom(1))
             }
 
             // Check whether the value of the collateral is higher.
@@ -60,7 +60,7 @@ pub fn process_instruction(
                 return Ok(())
             } else {
                 msg!("The value of loan is higher!");
-                return Err(ProgramError::Custom(1))
+                return Err(ProgramError::Custom(2))
             }
         }
     }
