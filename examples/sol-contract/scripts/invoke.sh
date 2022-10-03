017030bdf361bcce08010cb7c1b12e93d7f52ef3
Yunhao Zhang <yz2327@cornell.edu>
2022-10-03 09:44:27 -0400

    Cleanup the client-side code

--- examples/sol-contract/scripts/invoke.sh
+++ examples/sol-contract/scripts/invoke.sh
@@ -1,1 +1,1 @@
-cd scripts; npm install typescript; npm run build; node invoke.js `solana-keygen pubkey ../build/example_sol_contract-keypair.json`
+cd scripts; npm install typescript; npm run build; node invoke.js
