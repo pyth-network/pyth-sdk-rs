3cc600fc1fc454014c9e20c0d85590b714add565
Yunhao Zhang <yz2327@cornell.edu>
2022-10-09 19:42:24 -0400

    Normalize the values with the exponents

--- examples/sol-contract/src/processor.rs
+++ examples/sol-contract/src/processor.rs
@@ -71,40 +71,63 @@ pub fn process_instruction(
                 return Err(ProgramError::Custom(2));
             }
 
-            // Calculate the maximum value of the loan using Pyth.
+            // With high confidence, the maximum value of the loan is
+            // (price + conf) * loan_qty * 10 ^ (expo).
+            // Here is more explanation on confidence interval in Pyth:
+            // https://docs.pyth.network/consume-data/best-practices
             let feed1 = load_price_feed_from_account_info(pyth_loan_account)?;
             let result1 = feed1.get_current_price().ok_or(ProgramError::Custom(3))?;
             let loan_max_price = result1
                 .price
                 .checked_add(result1.conf as i64)
                 .ok_or(ProgramError::Custom(4))?;
-            let loan_max_value = loan_max_price
+            let mut loan_max_value = loan_max_price
                 .checked_mul(loan_qty)
                 .ok_or(ProgramError::Custom(4))?;
+            msg!(
+                "The maximum loan value is {} * 10^({}).",
+                loan_max_value,
+                result1.expo
+            );
 
-            // Calculate the minimum value of the collateral using Pyth.
+            // With high confidence, the minimum value of the collateral is
+            // (price - conf) * collateral_qty * 10 ^ (expo).
+            // Here is more explanation on confidence interval in Pyth:
+            // https://docs.pyth.network/consume-data/best-practices
             let feed2 = load_price_feed_from_account_info(pyth_collateral_account)?;
             let result2 = feed2.get_current_price().ok_or(ProgramError::Custom(3))?;
             let collateral_min_price = result2
                 .price
                 .checked_sub(result2.conf as i64)
                 .ok_or(ProgramError::Custom(4))?;
-            let collateral_min_value = collateral_min_price
+            let mut collateral_min_value = collateral_min_price
                 .checked_mul(collateral_qty)
                 .ok_or(ProgramError::Custom(4))?;
-
-            // Check whether the value of the collateral is higher.
-            msg!(
-                "The maximum loan value is {} * 10^({}).",
-                loan_max_value,
-                result1.expo
-            );
             msg!(
                 "The minimum collateral value is {} * 10^({}).",
                 collateral_min_value,
                 result2.expo
             );
 
+            // If the loan and collateral prices use different exponent,
+            // normalize the value.
+            if result1.expo > result2.expo {
+                let normalize = (10 as i64)
+                    .checked_pow((result1.expo - result2.expo) as u32)
+                    .ok_or(ProgramError::Custom(4))?;
+                collateral_min_value = collateral_min_value
+                    .checked_mul(normalize)
+                    .ok_or(ProgramError::Custom(4))?;
+            } else if result1.expo < result2.expo {
+                let normalize = (10 as i64)
+                    .checked_pow((result2.expo - result1.expo) as u32)
+                    .ok_or(ProgramError::Custom(4))?;
+                loan_max_value = loan_max_value
+                    .checked_mul(normalize)
+                    .ok_or(ProgramError::Custom(4))?;
+            }
+
+            // Check whether the value of the collateral is higher.
             if collateral_min_value > loan_max_value {
                 msg!("The value of the collateral is higher.");
                 return Ok(());
