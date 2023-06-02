43c583b2b2fd536ada31a1836ded895d2b98eb60
Guillermo Bescos <g.bescos@yahoo.com>
2023-06-02 12:45:56 +0100

    Cleanup the test

--- examples/sol-anchor-contract/tests/sol-anchor-contract.ts
+++ examples/sol-anchor-contract/tests/sol-anchor-contract.ts
@@ -2,11 +2,9 @@ import * as anchor from "@coral-xyz/anchor";
 import { SolAnchorContract } from "../target/types/sol_anchor_contract";
 
 describe("sol-anchor-contract", () => {
-  const fs = require("fs");
-  const assert = require("assert");
 
-  let ethToUSD = "EdVCmQ9FSPcVe5YySXDPCRmc8aDQLKJ9xvYBMZPie1Vw";
-  let usdtToUSD = "38xoQ4oeJCBrcVvca2cGk7iV1dAfrmTR1kmhSCJQ8Jto";
+  const ethToUSD = "EdVCmQ9FSPcVe5YySXDPCRmc8aDQLKJ9xvYBMZPie1Vw";
+  const usdtToUSD = "38xoQ4oeJCBrcVvca2cGk7iV1dAfrmTR1kmhSCJQ8Jto";
 
   const provider = anchor.AnchorProvider.local();
 
@@ -17,10 +15,7 @@ describe("sol-anchor-contract", () => {
   const program = anchor.workspace
     .SolAnchorContract as anchor.Program<SolAnchorContract>;
   const payer = provider.wallet.publicKey;
-  let programId = program.programId;
-  const programKey = program.programId;
-  console.log(programKey)
-
+  
   it("Initialize the config.", async () => {
     const tx = await program.methods
       .init({
@@ -28,7 +23,6 @@ describe("sol-anchor-contract", () => {
         collateralPriceFeedId: new anchor.web3.PublicKey(usdtToUSD),
       })
       .accounts({
-        program: programId,
         payer,
         config: config.publicKey,
         systemProgram: anchor.web3.SystemProgram.programId,
@@ -54,30 +48,4 @@ describe("sol-anchor-contract", () => {
     console.log("Loan2Value() is invoked: " + tx);
   });
 
-  it("Prevent initialization of config without authority.", async () => {
-    const attacker = anchor.web3.Keypair.generate();
-
-    var tx,
-      success = false;
-    try {
-      tx = await program.methods
-        .init({
-          loanPriceFeedId: new anchor.web3.PublicKey(ethToUSD),
-          collateralPriceFeedId: new anchor.web3.PublicKey(usdtToUSD),
-        })
-        .accounts({
-          program: programId,
-          payer,
-          config: config.publicKey,
-          systemProgram: anchor.web3.SystemProgram.programId,
-        })
-        .signers([config, attacker])
-        .rpc();
-
-      success = true;
-    } catch (error) {
-      console.log("Attacker failed at unauthorized init");
-    }
-    if (success) throw new Error("Attacker succeeded! TxHash: " + tx);
-  });
 });
